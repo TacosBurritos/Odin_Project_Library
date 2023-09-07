b67c9ace74277c6be8bbedb94fd2b02256b6304f
Takafumi <tmashiko7@gmail.com>
2023-09-07 09:50:11 -0400

    second edit

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ function Book(title, author, pages, read){
         return title + ' ' + ' by  ' + author + ", " + pages + ' pages, ' + read;
     }
 }
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-console.log(book1.info());
+//showing that I can console.log object.info() in order to print out the contents of the book
+
+// console.log(book1.info());
+let library = [];
+//must write a code that logs or stores the value of new objects that the user inputs
+let books;
+function addBookToLibrary(){
+    const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+    library.push(book1);
+}
+
+function display(){
+    let content = document.querySelector(".content");
+    let div = document.createElement("div");
+    div.classList.add("eachBook");
+    for(let i = 0; i < library.length; i++){
+        div.textContent = library[i];
+        content.appendChild(div);
+    }
+}
